0e856cfe1411b477ed2f3a07a3349c554d03043b
Fauzan-affan <fauzanaffanzaki93@gmail.com>
2021-04-21 14:56:37 +0700

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,26 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Hello World"/>
+  <Contact />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Contact from './components/Contact.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Contact
+  },
+  data() {
+    return {
+
+    }
+  },
+  template: {
+    
   }
 }
 </script>
---
+++ src/components/Contact.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>Contact: {{contact}}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "contact",
+        data() {
+            return {
+                contact: "081311688811"
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
